9636ee3fba7e01c39d32dfb3a355b8e7dcb0ec5d
muhammad-madridista <30875187+muhammad-madridista@users.noreply.github.com>
2021-05-19 00:12:28 +0530

    Update CoupanIdNotFoundException.java

--- OnlinePizzaApplication/OnlinePizzaApplication/src/main/java/com/cg/pizza/exception/CoupanIdNotFoundException.java
+++ OnlinePizzaApplication/OnlinePizzaApplication/src/main/java/com/cg/pizza/exception/CoupanIdNotFoundException.java
@@ -1,19 +1,17 @@
-package com.cg.pizza.exception;
-
 import java.util.Date;
 
 public class CoupanIdNotFoundException extends RuntimeException {
 	private static final long serialVersionUID = 1L;
 	private Date timestamp;
-	private String message;
+	private String coupan;
 public CoupanIdNotFoundException() {
 	// TODO Auto-generated constructor stub
 }
-public CoupanIdNotFoundException(String message) {
-	super(message);
+public CoupanIdNotFoundException(String coupan) {
+	super(coupan);
 }
 @Override
 public String toString() {
-	return "CoupanIdNotFoundException [timestamp=" + timestamp + ", message=" + message + "]";
+	return "CoupanIdNotFoundException [timestamp=" + timestamp + ", coupan=" + coupan + "]";
 }
 }
