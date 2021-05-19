f2d56fe7f312aa30f60811e2813850921147cd2f
muhammad-madridista <30875187+muhammad-madridista@users.noreply.github.com>
2021-05-19 09:45:10 +0530

    Update CoupanService.java

--- OnlinePizzaApplication/OnlinePizzaApplication/src/main/java/com/cg/pizza/service/CoupanService.java
+++ OnlinePizzaApplication/OnlinePizzaApplication/src/main/java/com/cg/pizza/service/CoupanService.java
@@ -14,26 +14,26 @@ public class CoupanService implements ICoupanService{
 	@Autowired
 	private CoupanRepository coupanRepository;
 	
-	public List<Coupan> viewCoupan()
+	public List<Coupan> viewCoupans()
 	{
 		List<Coupan> coupan = coupanRepository.findAll();
 		return coupan;
 	}
 	
-	public Coupan viewCoupan(int id) {
+	public Coupan viewCoupans(int id) {
 		Optional<Coupan> coupan = coupanRepository.findById(id);
 		return (coupan.isPresent()) ? coupan.get() : null;
 	}
 	
-	public Coupan deleteCoupan(int id) {
-		Coupan coupan = viewCoupan(id);
+	public Coupan deleteCoupans(int id) {
+		Coupan coupan = viewCoupans(id);
 		if (coupan!= null)
 			coupanRepository.deleteById(id);
 		return coupan;
 	}
 	
-	public Coupan editCoupan(Coupan coupan) {
-	    Coupan coupanexist = viewCoupan(coupan.getCoupanId());
+	public Coupan editCoupans(Coupan coupan) {
+	    Coupan coupanexist = viewCoupans(coupan.getCoupanId());
 		if (coupanexist != null) {
 			coupan = coupanRepository.save(coupan);
 		}
@@ -41,8 +41,8 @@ public class CoupanService implements ICoupanService{
 	}
 
 
-	public Coupan insertCoupan(Coupan coupan) {
-		Coupan coupanexist = viewCoupan(coupan.getCoupanId());
+	public Coupan addCoupans(Coupan coupan) {
+		Coupan coupanexist = viewCoupans(coupan.getCoupanId());
 		if (coupanexist == null) {
 			coupan = coupanRepository.save(coupan);
 		}
