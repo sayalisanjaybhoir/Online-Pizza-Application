134cc2ae80e7eb4e72976955afab2155337ee356
Satyam nayak <42289567+satyamn1112@users.noreply.github.com>
2021-05-20 00:56:14 +0530

    Add files via upload

---
+++ OnlinePizzaApplication/OnlinePizzaApplication/src/main/java/com/cg/pizza/exception/UserExistsException.java
@@ -0,0 +1,9 @@
+package com.cg.pizza.exception;
+
+public class UserExistsException extends RuntimeException {
+	public UserExistsException(String msg) {
+		// TODO Auto-generated constructor stub
+		super(msg);
+	}
+
+}
---
+++ OnlinePizzaApplication/OnlinePizzaApplication/src/main/java/com/cg/pizza/exception/UserNotFoundException.java
@@ -0,0 +1,9 @@
+package com.cg.pizza.exception;
+
+public class UserNotFoundException extends RuntimeException {
+	public UserNotFoundException(String msg) {
+		// TODO Auto-generated constructor stub
+		super(msg);
+	}
+
+}
