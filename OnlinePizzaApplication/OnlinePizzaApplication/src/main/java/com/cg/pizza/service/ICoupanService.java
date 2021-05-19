5c61b504f5f226efa2cd80c743418d61f3d99557
muhammad-madridista <30875187+muhammad-madridista@users.noreply.github.com>
2021-05-19 09:35:28 +0530

    Update ICoupanService.java

--- OnlinePizzaApplication/OnlinePizzaApplication/src/main/java/com/cg/pizza/service/ICoupanService.java
+++ OnlinePizzaApplication/OnlinePizzaApplication/src/main/java/com/cg/pizza/service/ICoupanService.java
@@ -2,7 +2,15 @@ package com.cg.pizza.service;
 
 import java.util.*;
 import com.cg.pizza.entity.Coupan;
+import com.cg.pizza.exception.CoupanIdNotFoundException;
+import com.cg.pizza.exception.InvalidCoupanOperationException;
 
 public interface ICoupanService {
-	public List<Coupan> viewCoupan();
+	public Coupan addCoupans(Coupan coupan);
+
+	public Coupan editCoupans(Coupan coupan)throws InvalidCoupanOperationException;
+
+	public Coupan deleteCoupans(int coupanId)throws CoupanIdNotFoundException;
+
+	public List<Coupan> viewCoupans();
 }
