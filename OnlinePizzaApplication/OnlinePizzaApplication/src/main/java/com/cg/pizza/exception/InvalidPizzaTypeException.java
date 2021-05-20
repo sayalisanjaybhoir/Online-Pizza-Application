6bea3c015b21cb590a14bf246c926fee807e2953
sahillohani <36572619+sahillohani@users.noreply.github.com>
2021-05-20 15:16:46 +0530

    Add files via upload

---
+++ OnlinePizzaApplication/OnlinePizzaApplication/src/main/java/com/cg/pizza/exception/InvalidPizzaTypeException.java
@@ -0,0 +1,26 @@
+package com.cg.pizza.exception;
+
+import java.util.Date;
+
+public class InvalidPizzaTypeException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+  
+	private Date timestamp;
+	private String message;
+
+	public InvalidPizzaTypeException() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public InvalidPizzaTypeException(String message) {
+		super(message);
+	}
+
+	@Override
+	public String toString() {
+		return "InvalidPizzaTypeException [timestamp=" + timestamp + ", message=" + message + "]";
+	}
+	
+	
+	
+}
