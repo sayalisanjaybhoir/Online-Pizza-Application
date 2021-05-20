3cf4951785a38bf3166e2df0d4f4ca3134069b30
muhammad-madridista <30875187+muhammad-madridista@users.noreply.github.com>
2021-05-20 15:07:47 +0530

    Update CoupanController.java

--- OnlinePizzaApplication/OnlinePizzaApplication/src/main/java/com/cg/pizza/controller/CoupanController.java
+++ OnlinePizzaApplication/OnlinePizzaApplication/src/main/java/com/cg/pizza/controller/CoupanController.java
@@ -12,13 +12,18 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import java.net.URI;
 import java.util.*;
+
+import javax.validation.Valid;
+
 import com.cg.pizza.service.CoupanService;
 
 import com.cg.pizza.entity.Coupan;
 import com.cg.pizza.exception.CoupanIdNotFoundException;
+import com.cg.pizza.exception.CoupanNameNotFoundException;
 import com.cg.pizza.exception.InvalidCoupanOperationException;
 
 @RestController
@@ -26,15 +31,17 @@ import com.cg.pizza.exception.InvalidCoupanOperationException;
 public class CoupanController {
       @Autowired
       private CoupanService coupanService;
+      Logger logger = LoggerFactory.getLogger(CoupanController.class);
       
+//1 Get all coupans  
       @GetMapping("/coupan")
       public ResponseEntity<List<Coupan>> viewCoupans() {
 
       	List<Coupan> viewCoupanList = coupanService.viewCoupans();
 		// Creating an error response.
       	ResponseEntity<List<Coupan>> response = new ResponseEntity<>(viewCoupanList, HttpStatus.NOT_FOUND);
-
-      	// If messageList is not empty it sets the list in response else by default
+      	
+      	// If coupanList is not empty it sets the list in response else by default
       	// error will be
       	// there in response.
       	if (!viewCoupanList.isEmpty()) {
@@ -44,21 +51,35 @@ public class CoupanController {
       	return response;
       }
       
-      
+    //2 Get coupan by id     
       @GetMapping(value = "/{coupanId}")
-  	public ResponseEntity<Object> viewCoupans(@PathVariable("coupanId") int coupanId) {
+  	public ResponseEntity<Object> viewCoupans(@Valid@PathVariable("coupanId") int coupanId) {
 
-  		Coupan coupan = coupanService.viewCoupans(coupanId);
+    	  Coupan coupan = coupanService.viewCoupans(coupanId);	
+  		 if(coupanId<0 ) {
+    		 logger.error("Coupan id cant be negative");
+    		throw new CoupanIdNotFoundException( "Coupan Id is not negative");
+    	 }  		 
+//  		 else if  (!System.coupanName.RegularExpressions.Regex.IsMatch("^[0-9]", coupan))
+//  
+//  		 {
+//  			throw new CoupanIdNotFoundException( "Coupan name  cannot be numeric");
+//         }
+//     }
+  		
   		// Creating an error response.
   		//ResponseEntity<Object> response = ResponseEntity.status(HttpStatus.BAD_REQUEST)
   	//			.body("Coupan " + coupanId + " Not found");
 
-  		// If message is not null it sets the message object in response else by default
+  		// If coupan is not null it sets the coupan object in response else by default
   		// error
   		// will be there in response.
-  		if (coupan == null) {
+  		 
+
+  		 else if (coupan == null) {
   			//response = new ResponseEntity<>(coupan, HttpStatus.OK);
-  			throw new CoupanIdNotFoundException("Coupan ID" + coupanId + "Not Found");
+  			logger.error(" Coupan Id Not Found Exception");
+  			throw new CoupanIdNotFoundException("Coupan ID"  + coupanId +  "Not Found");
   		}
 
   		//return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Coupan" + coupanId + "Not Found");
@@ -66,28 +87,53 @@ public class CoupanController {
   	}
 
       
+ //3 Get Coupan by name
+      @GetMapping("/coupan/{coupanName}")
+      public ResponseEntity<Object> viewCoupanList(@PathVariable("coupanName") String coupanName) {
+  		logger.info("Inside view pizza by type method");
+
+  		List<Coupan> bycoupanName = coupanService.viewCoupanList(coupanName);
+  		
+      if (bycoupanName.isEmpty() || bycoupanName==null) {
+       throw new CoupanNameNotFoundException("Enter proper coupan name");
+}
+//      else if(!System.bycoupanName.RegularExpressions.Regex.IsMatch("^[0-9]", bycoupanName) {
+//    	  throw new CoupanNameNotFoundException("Coupan Name cannot be an integer")
+//      }
+      return new ResponseEntity<>(bycoupanName, HttpStatus.OK); 
+      }
+      
+//4 Delete the coupan by id      
      @DeleteMapping("/{coupanId}")
  	public ResponseEntity<Object> deleteCoupans(@PathVariable("coupanId") int coupanId) {
- 		// If message is deleted it returns deleted message object else null
- 		Coupan coupanPresent = coupanService.deleteCoupans(coupanId);
+ 		// If coupan is deleted it returns deleted coupan object else null
+ 		Coupan coupanPresent = coupanService.deleteCoupans(coupanId);	
+  		 if(coupanId<0) {
+    		 logger.error("Coupan id cant be negative");
+    		throw new CoupanIdNotFoundException("Coupan Id is not negative");
+    	 }
  		// Creating an success response.
- 		ResponseEntity<Object> response = ResponseEntity.status(HttpStatus.OK)
- 				.body("Coupan " + coupanId + " deleted");
- 		// response is set to error if message is null.
- 		if (coupanPresent == null) {
+// 		ResponseEntity<Object> response = ResponseEntity.status(HttpStatus.OK)
+// 				.body("Coupan " + coupanId + " deleted");
+ 		// response is set to error if coupan is null.
+  		 else if (coupanPresent == null) {
  			//response = ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Coupan " + coupanId + " Not found");
  			throw new CoupanIdNotFoundException("Coupan ID" + coupanId + "Not Found");
  		}
  		return ResponseEntity.status(HttpStatus.OK).body("Coupan " + coupanId + "Deleted");
  	}
+
      
+     
+     
+//5 Update the coupan by id     
      @PutMapping("/{coupanId}")
      public ResponseEntity<Object> editCoupans(@PathVariable("coupanId") int coupanId,
  			@RequestBody Coupan coupan) {
  		coupan.setCoupanId(coupanId);
- 		// If message is updated it returns updates message object else null
+ 		// If coupan is updated it returns updates coupan object else null
  		Coupan editCoupan = coupanService.editCoupans(coupan);
- 		// response is set to error if message is null.
+ 		// response is set to error if coupan is null.
  		if (editCoupan==null) {
  			//return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Coupan " + coupanId + " Not found");
  			throw new CoupanIdNotFoundException("Coupan ID" + coupanId + "Not Found");
@@ -101,17 +147,23 @@ public class CoupanController {
  	}
 
      
-     
+//6 Insert the coupan     
      @PostMapping
      public ResponseEntity<Object> addCoupans(@RequestBody Coupan coupan) {
-    	 
+    	 logger.debug("Coupan No is coming here"+coupan.getCoupanId());
+    	
     	 if (coupan.getCoupanName().isEmpty() || coupan.getCoupanName()==null) {
-    		 
+    		 logger.error("Coupan Name is not here");
+   
   			throw new InvalidCoupanOperationException(coupan + "Again enter the coupan Name");
   		}
- 		Coupan newCoupan = coupanService.addCoupans(coupan);
+    	 else  if(coupan.getCoupanId()<0) {
+    		 logger.error("Coupan id is coming here");
+    		 throw new CoupanIdNotFoundException("Coupan Id cannot be negative");
+    	 }
+    	 Coupan newCoupan = coupanService.addCoupans(coupan); 
  		
- 		if (coupan==null) {
+ 		if (newCoupan==null) {
  			 
  			throw new InvalidCoupanOperationException(coupan + "Again enter the coupan");
  		}
@@ -124,3 +176,8 @@ public class CoupanController {
  		
  	}
       
+
+
+
+
+
