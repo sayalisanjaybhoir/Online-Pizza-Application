845108ad6aaf5656df7483a242f67c2d9dab968f
sahillohani <36572619+sahillohani@users.noreply.github.com>
2021-05-20 15:09:12 +0530

    Update IPizzaService.java

--- OnlinePizzaApplication/OnlinePizzaApplication/src/main/java/com/cg/pizza/service/IPizzaService.java
+++ OnlinePizzaApplication/OnlinePizzaApplication/src/main/java/com/cg/pizza/service/IPizzaService.java
@@ -1,8 +1,10 @@
 package com.cg.pizza.service;
-import com.cg.pizza.entity.Pizza;
-import com.cg.pizza.exception.PizzaIdNotFoundException;
 
 import java.util.List;
+
+import com.cg.pizza.entity.Pizza;
+import com.cg.pizza.exception.InvalidMinCostException;
+import com.cg.pizza.exception.PizzaIdNotFoundException;
 
 public interface IPizzaService {
 	Pizza addPizza(Pizza pizza);
@@ -15,6 +17,8 @@ public interface IPizzaService {
 
 	List<Pizza> viewPizzaList();
 
-	//List<Pizza> viewPizzaList(double minCost, double maxCost)throws InvalidMinCostException;
+	List<Pizza> viewPizzaList(double minCost, double maxCost) throws InvalidMinCostException;
+
+	List<Pizza> viewPizzaList(String pizzaType);
 
 }
