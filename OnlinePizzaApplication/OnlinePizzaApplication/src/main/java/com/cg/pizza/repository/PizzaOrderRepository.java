634894f9a8766a516ad0802ead7f6d58031b7d30
sayalisanjaybhoir <83551370+sayalisanjaybhoir@users.noreply.github.com>
2021-05-21 11:08:43 +0530

    Update PizzaOrderRepository.java

--- OnlinePizzaApplication/OnlinePizzaApplication/src/main/java/com/cg/pizza/repository/PizzaOrderRepository.java
+++ OnlinePizzaApplication/OnlinePizzaApplication/src/main/java/com/cg/pizza/repository/PizzaOrderRepository.java
@@ -1,25 +1,17 @@
 package com.cg.pizza.repository;
 
-
-
 import java.time.LocalDate;
 import java.util.List;
 
-
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
 import com.cg.pizza.entity.PizzaOrder;
 
-
 @Repository
-public interface PizzaOrderRepository extends JpaRepository<PizzaOrder, Integer>
-{
-	//@Query("select p from PizzaOrder as p where p.orderDate =:date ")
+public interface PizzaOrderRepository extends JpaRepository<PizzaOrder, Integer> {
+	
 	public List<PizzaOrder> orderDate(LocalDate orderDate);
-	
-	
 
 }
+
