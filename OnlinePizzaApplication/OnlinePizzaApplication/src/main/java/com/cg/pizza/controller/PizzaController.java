cbcd7f989b1bc931ad35d8b651f004255b728780
sahillohani <36572619+sahillohani@users.noreply.github.com>
2021-05-21 11:21:57 +0530

    Update PizzaController.java

--- OnlinePizzaApplication/OnlinePizzaApplication/src/main/java/com/cg/pizza/controller/PizzaController.java
+++ OnlinePizzaApplication/OnlinePizzaApplication/src/main/java/com/cg/pizza/controller/PizzaController.java
@@ -1,6 +1,5 @@
 package com.cg.pizza.controller;
 
-import java.net.URI;
 import java.util.List;
 
 import org.slf4j.Logger;
@@ -16,8 +15,6 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-
 import com.cg.pizza.entity.Pizza;
 import com.cg.pizza.exception.InvalidMinCostException;
 import com.cg.pizza.exception.InvalidPizzaOperationException;
@@ -38,14 +35,10 @@ public class PizzaController {
 
 		logger.info("Get all pizza list");
 		List<Pizza> pizzaList = pizzaService.viewPizzaList();
-		// Creating an error response.
 		ResponseEntity<List<Pizza>> response = new ResponseEntity<>(pizzaList, HttpStatus.NOT_FOUND);
-//If Pizza is not empty it sets the list in response else by default,error will be in response.
-
 		if (!pizzaList.isEmpty()) {
 			response = new ResponseEntity<>(pizzaList, HttpStatus.OK);
 		}
-
 		return response;
 	}
 
@@ -53,11 +46,9 @@ public class PizzaController {
 	public ResponseEntity<Object> viewPizzaById(@PathVariable("pizzaId") int pizzaId) {
 		logger.info("Search by Pizza Id");
 		Pizza pizza = pizzaService.viewPizza(pizzaId);
-
 		if (pizza == null) {
 			logger.error("Pizza ID Not Found Exception");
 			throw new PizzaIdNotFoundException(pizzaId + " This id is not available");
-
 		}
 		return new ResponseEntity<>(pizza, HttpStatus.OK);
 	}
@@ -74,24 +65,23 @@ public class PizzaController {
 		} else
 
 			return ResponseEntity.status(HttpStatus.OK).body("pizza " + pizzaId + " deleted");
-
 	}
 
 	@PostMapping
 	public ResponseEntity<Object> addPizza(@RequestBody Pizza pizza) {
 		logger.info("Inside Post add method");
-		if ((pizza.getPizzaName().isBlank() || pizza.getPizzaDescription().isBlank()) || pizza.getPizzaId() < 1   || (pizza.getPizzaCost() < 1 ||  pizza.getPizzaCostAfterCoupan() < 1)) 
+		if ((pizza.getPizzaName().isEmpty() || pizza.getPizzaDescription().isEmpty()) || (pizza.getPizzaCost() < 1 ||  pizza.getPizzaCostAfterCoupan() < 1)) 
 		{
 			logger.error("entered values are null or incorrect");
 			throw new InvalidPizzaOperationException(" Enter correct Pizza details");
-		}	
-					
-		
-		
+			
+		}			
+			else  if(pizza.getPizzaId()<0) {
+	    		 logger.error("Coupan id is negative");
+	    		 throw new PizzaIdNotFoundException("Pizza Id cannot be negative");
+		}
 		 else  {
-
 			pizzaService.addPizza(pizza);
-
 			return ResponseEntity.status(HttpStatus.OK).body("pizza added");
 		}
 	}
@@ -99,22 +89,23 @@ public class PizzaController {
 	@PutMapping("/{pizzaId}")
 	public ResponseEntity<Object> editPizza(@PathVariable("pizzaId") int pizzaId, @RequestBody Pizza pizza) {
 		logger.info("Inside Put method of Pizza");
+		if ((pizza.getPizzaName().isEmpty() || pizza.getPizzaDescription().isEmpty()) || (pizza.getPizzaCost() < 1 ||  pizza.getPizzaCostAfterCoupan() < 1)) 
+		{
+			logger.error("entered values are null or incorrect");
+			throw new InvalidPizzaOperationException(" Enter correct Pizza details"); }
+			
+		else  if(pizza.getPizzaId()<0) {
+   		 logger.error("Coupan id is negative");
+   		 throw new PizzaIdNotFoundException("Pizza Id cannot be negative"); }
 
-		pizza.setPizzaId(pizzaId);
-
-		Pizza editPizza = pizzaService.updatePizza(pizza);
-		if (editPizza == null) {
-			logger.error("entered value are null ");
-			throw new InvalidPizzaOperationException(" Enter correct Pizza details");
-		//	return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Pizza " + pizzaId + " Not found");
-		} else {
-			// response is set to updated Pizza id in response header section.
-			URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
-					.buildAndExpand(pizza.getPizzaId()).toUri();
-
-			return ResponseEntity.created(location).build();
+		 else {
+						
+			pizza.setPizzaId(pizzaId);
+			pizzaService.updatePizza(pizza);
+		 }
+		return ResponseEntity.status(HttpStatus.OK).body("Pizza");
 		}
-	}
+	
 
 	@GetMapping("/between/{minimumcost}/{maximumcost}")
 
@@ -128,7 +119,7 @@ public class PizzaController {
 
 			throw new InvalidMinCostException("Please check the cost given ");
 		} else
-
+           
 			return pizList;
 	}
 
@@ -147,11 +138,6 @@ public class PizzaController {
 			logger.error("enter correect pizza type");
 			throw new InvalidPizzaTypeException("Pizza type can only be veg or non-veg ");
 		}
-			
-		//List<Pizza> pizTypeList = pizzaService.viewPizzaList(pizzaType);
-
-		
-	
 	
 	
 	}
