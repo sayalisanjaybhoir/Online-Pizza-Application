cce0da8244eaba3b7984ac0179cadb63493877f2
muhammad-madridista <30875187+muhammad-madridista@users.noreply.github.com>
2021-05-21 13:57:54 +0530

    Update Coupan.java

--- OnlinePizzaApplication/OnlinePizzaApplication/src/main/java/com/cg/pizza/entity/Coupan.java
+++ OnlinePizzaApplication/OnlinePizzaApplication/src/main/java/com/cg/pizza/entity/Coupan.java
@@ -1,22 +1,31 @@
 package com.cg.pizza.entity;
 
+import java.io.Serializable;
 import java.util.HashSet;
 import java.util.Set;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
-import javax.persistence.Table;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.Min;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties({"hibernateLazyInitilizer","handler"})
 @Entity
 @Table(name="coupandetails")
-public class Coupan {
+
+public class Coupan implements Serializable{
+	private static final long serialVersionUID = 1L;
       @Id
-      @GeneratedValue
       private int coupanId;
       @Column(name=" coupanName")
       private String coupanName;
@@ -25,14 +34,24 @@ public class Coupan {
       @Column(name=" coupanPizzaId")
       private int coupanPizzaId;
       
-      @ManyToMany
+      @JsonIgnore
+      @ManyToMany(fetch = FetchType.LAZY,cascade = {CascadeType.ALL})
       @JoinTable(name = "Coupan_PizzaOrder", joinColumns = { @JoinColumn(name = "coupanId") }, inverseJoinColumns = {
   			@JoinColumn(name = "bookingOrderId") })
     // PizzaOrder pizzaorder;
       Set<PizzaOrder> pizzaOrderSet = new HashSet<PizzaOrder>();
       
-      public Coupan() {}
-//Getter and Setter Methods     
+      public Coupan() {
+    	  
+      }
+public Coupan(int coupanId, String coupanName, String coupanDescription, int coupanPizzaId) {
+		super();
+		this.coupanId = coupanId;
+		this.coupanName = coupanName;
+		this.coupanDescription = coupanDescription;
+		this.coupanPizzaId = coupanPizzaId;
+	}
+	//Getter and Setter Methods     
 	public int getCoupanId() {
 		return coupanId;
 	}
@@ -70,7 +89,4 @@ public class Coupan {
 	}
 	
 	
-      
-      
-      
-}
+ }
