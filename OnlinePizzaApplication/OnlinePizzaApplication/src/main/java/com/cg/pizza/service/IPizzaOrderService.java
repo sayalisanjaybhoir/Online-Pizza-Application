d763ecf6784878c56f9666944a9c4926ae5d302f
sayalisanjaybhoir <83551370+sayalisanjaybhoir@users.noreply.github.com>
2021-05-21 11:07:38 +0530

    Update IPizzaOrderService.java

--- OnlinePizzaApplication/OnlinePizzaApplication/src/main/java/com/cg/pizza/service/IPizzaOrderService.java
+++ OnlinePizzaApplication/OnlinePizzaApplication/src/main/java/com/cg/pizza/service/IPizzaOrderService.java
@@ -5,21 +5,14 @@ import java.util.List;
 
 import com.cg.pizza.entity.PizzaOrder;
 
-public interface IPizzaOrderService 
-{
+public interface IPizzaOrderService {
 
-	//1
 	public PizzaOrder bookPizzaOrder(PizzaOrder pizzaOrder);
-	//2
 	public PizzaOrder updatePizzaOrder(PizzaOrder pizzaOrder);
-	//3
 	public PizzaOrder cancelPizzaOrder(int bookingOrderId);
-	//4
 	public PizzaOrder viewPizzaOrder(int bookingOrderId);
-	//5 
 	public List<PizzaOrder> viewOrderList();
-	//6 view List By Date
 	public List<PizzaOrder> viewOrderByDate(LocalDate date);
-	//7 calculate
-	public List<PizzaOrder> calculateTotal(String size , int quantity );
+	public List<PizzaOrder> calculateTotal(int id, String size, int quantity);
+
 }
